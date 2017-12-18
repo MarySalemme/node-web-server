f6923b58f6f07211667b815ad0a235bc9ea5fe3e
MarySalemme <marysalemme1@gmail.com>
2017-12-18 17:30:45 +0000

    Add express middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
@@ -7,8 +8,25 @@ const app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
+
 // express middleware: lets you configure hou your application will work 
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+   const now = new Date().toString();
+   const log = `${now}: ${req.method} ${req.url}`;
+   console.log(log);
+   fs.appendFile('server.log', log + '\n', (err) => {
+      if (err) {
+         console.log('Unable to append to server.log');
+      }
+   })
+   next();
+})
+
+// app.use((req, res, next) => {
+//    res.render('ma intenance.hbs');
+// })
 
 hbs.registerHelper('getCurrentYear', () => {
    return new Date().getFullYear();
